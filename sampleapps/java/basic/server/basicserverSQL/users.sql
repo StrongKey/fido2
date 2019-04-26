f5cdd136eff80217509690b4eb3568edee112e9d
Michael Ishimoto <michael.ishimoto@strongkey.com>
2019-04-25 17:03:50 -0700

    FIDO2 + Email POC

---
+++ sampleapps/java/basic/server/basicserverSQL/users.sql
@@ -0,0 +1,18 @@
+/*
+ * Copyright StrongAuth, Inc. All Rights Reserved.
+ * 
+ * Use of this source code is governed by the Gnu Lesser General Public License 2.3.
+ * The license can be found at https://github.com/StrongKey/fido2/LICENSE
+ */
+create table IF NOT EXISTS Users (
+        id                              bigint unsigned not null,
+        email                      	varchar(64) not null,
+        username                        varchar(64) not null,
+        firstname        		varchar(32) not null,
+        lastname                        varchar(32) not null,
+        create_date                     DATETIME not null,
+                primary key (id),
+                unique index (email),
+                unique index (username)
+        )
+engine=innodb;
---
+++ sampleapps/java/basic/server/src/main/java/com/strongkey/utilities/Common.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright StrongAuth, Inc. All Rights Reserved.
+ * 
+ * Use of this source code is governed by the Gnu Lesser General Public License 2.3.
+ * The license can be found at https://github.com/StrongKey/FIDO-Server/LICENSE
+ */
+package com.strongkey.utilities;
+
+import java.io.StringReader;
+import java.util.logging.Level;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import javax.json.stream.JsonParsingException;
+import javax.xml.ws.WebServiceException;
+
+public class Common {
+    private static final String CLASSNAME = Common.class.getName();
+    
+    // Verify that SKFE response is the proper format
+    public static JsonObject parseJsonFromString(String responseJsonString){
+        try (JsonReader jsonReader = Json.createReader(new StringReader(responseJsonString))) {
+            return jsonReader.readObject();
+        }
+        catch(JsonParsingException ex){
+            WebauthnTutorialLogger.logp(Level.SEVERE, CLASSNAME, "verifyJson",
+                    "WEBAUTHN-ERR-5001", ex.getLocalizedMessage());
+            throw new WebServiceException(WebauthnTutorialLogger.getMessageProperty("WEBAUTHN-ERR-5001"));
+        }
+    }
+}
