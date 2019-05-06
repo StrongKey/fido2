463d7708cae5e0c67c61da70c070c989ff229b4d
Michael Ishimoto <michael.ishimoto@strongkey.com>
2019-05-06 15:08:22 -0700

    First draft installation instructions

--- sampleapps/java/basic/server/basicserverInstall/install-basicserver.sh
+++ sampleapps/java/basic/server/basicserverInstall/install-basicserver.sh
@@ -10,9 +10,21 @@
 
 STRONGKEY_HOME=/usr/local/strongkey
 MARIATGT=mariadb-10.2.13
+GLASSFISH_HOME=$STRONGKEY_HOME/payara41/glassfish
 SKFS_SOFTWARE=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 MARIA_SKFSDBUSER_PASSWORD=AbracaDabra
 
+# Create DB Tables
 cd $SKFS_SOFTWARE/basicserverSQL
 $STRONGKEY_HOME/$MARIATGT/bin/mysql --user=skfsdbuser --password=$MARIA_SKFSDBUSER_PASSWORD --database=skfs --quick < create.txt
+
+# Create basic configurations
+touch $STRONGKEY_HOME/webauthntutorial/etc/webauthntutorial.properties
+echo "webauthntutorial.cfg.property.apiuri=https://$(hostname):8181/api" > $STRONGKEY_HOME/webauthntutorial/etc/webauthntutorial.properties
+chown strongkey:strongkey $STRONGKEY_HOME/webauthntutorial/etc/webauthntutorial.properties
+
+# Deploy sample application
+echo "Deploying StrongKey FidoServer ..."
+cp $SKFS_SOFTWARE/basicserver.war /tmp
+$GLASSFISH_HOME/bin/asadmin deploy /tmp/basicserver.war
