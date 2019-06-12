aec04cf006e2166cf474a9677c8f9a26d000df52
Pushkar Marathe <pushkar.marathe@strongkey.com>
2019-06-12 15:28:12 -0700

    rebasing

--- server/fidoserverInstall/install-skfs.sh
+++ server/fidoserverInstall/install-skfs.sh
@@ -23,6 +23,7 @@ BUFFERPOOLSIZE=512m
 
 # Flags to indicate if a module should be installed
 INSTALL_GLASSFISH=Y
+INSTALL_OPENDJ=Y
 INSTALL_MARIA=Y
 INSTALL_FIDO=Y
 
@@ -31,7 +32,11 @@ GLASSFISH=payara-4.1.2.181.zip
 JEMALLOC=jemalloc-3.6.0-1.el7.x86_64.rpm
 MARIA=mariadb-10.2.13-linux-x86_64.tar.gz
 MARIACONJAR=mariadb-java-client-2.2.2.jar
+OPENDJ=OpenDJ-3.0.0.zip
 # End Required Distributables
+
+SERVICE_LDAP_BIND_PASS=Abcd1234!
+SERVICE_LDAP_BASEDN='dc=strongauth,dc=com'
 
 # Other vars
 STRONGKEY_HOME=/usr/local/strongkey
@@ -41,7 +46,11 @@ GLASSFISH_CONFIG=$GLASSFISH_HOME/domains/domain1/config
 MARIAVER=mariadb-10.2.13-linux-x86_64
 MARIATGT=mariadb-10.2.13
 MARIA_HOME=$STRONGKEY_HOME/$MARIATGT
+OPENDJVER=opendj
+OPENDJTGT=OpenDJ-3.0.0
+OPENDJ_HOME=$STRONGKEY_HOME/$OPENDJTGT
 SKFS_SOFTWARE=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+SKCE_BASE_LDIF=skce-base.ldif
 
 function check_exists {
 for ARG in "$@"
@@ -115,6 +124,11 @@ if [ ! -f $SKFS_SOFTWARE/$JEMALLOC ]; then
 fi
 
 
+if [ ! -f $SKFS_SOFTWARE/$JEMALLOC ]; then
+        echo "Downloading OpenDJ ..."
+        wget https://github.com/OpenRock/OpenDJ/releases/download/3.0.0/OpenDJ-3.0.0.zip -q
+fi
+
 # Make sure we can resolve our own hostname
 get_ip "$(hostname)" > /dev/null
 
@@ -171,8 +185,9 @@ EOFSUDOERS
 cat > /etc/skfsrc << EOFSKFSRC
     export GLASSFISH_HOME=$GLASSFISH_HOME
         export MYSQL_HOME=$MARIA_HOME
+        export OPENDJ_HOME=$OPENDJ_HOME
    export STRONGKEY_HOME=$STRONGKEY_HOME
-              export PATH=\$GLASSFISH_HOME/bin:\$MYSQL_HOME/bin:\$STRONGKEY_HOME/bin:/usr/lib64/qt-3.3/bin:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin:/root/bin
+              export PATH=\$OPENDJ_HOME/bin:\$GLASSFISH_HOME/bin:\$MYSQL_HOME/bin:\$STRONGKEY_HOME/bin:/usr/lib64/qt-3.3/bin:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin:/root/bin
 
 alias str='cd $STRONGKEY_HOME'
 alias dist='cd $STRONGKEY_HOME/dist'
@@ -304,9 +319,62 @@ if [ $INSTALL_GLASSFISH = 'Y' ]; then
         cp $SKFS_SOFTWARE/$MARIACONJAR $GLASSFISH_HOME/lib
 fi
 
+if [ $INSTALL_OPENDJ = 'Y' ]; then
+        echo "Installing OpenDJ..."
+        if [ $SHOWALL ]; then
+                unzip $SKFS_SOFTWARE/$OPENDJ -d $STRONGKEY_HOME
+        else
+                unzip $SKFS_SOFTWARE/$OPENDJ -d $STRONGKEY_HOME > /dev/null
+        fi
+
+        mv $STRONGKEY_HOME/$OPENDJVER $OPENDJ_HOME
+
+        cp $SKFS_SOFTWARE/99-user.ldif $OPENDJ_HOME/template/config/schema
+
+        export "OPENDJ_JAVA_HOME=$JAVA_HOME"
+        if [ $SHOWALL ]; then
+                $OPENDJ_HOME/setup --cli --acceptLicense --no-prompt \
+                                   --ldifFile $SKFS_SOFTWARE/$SKCE_BASE_LDIF \
+                                   --rootUserPassword $SERVICE_LDAP_BIND_PASS \
+                                   --baseDN $SERVICE_LDAP_BASEDN \
+                                   --hostname $(hostname) \
+                                   --ldapPort 1389 \
+                                   --doNotStart
+        else
+                $OPENDJ_HOME/setup --cli --acceptLicense --no-prompt \
+                                   --ldifFile $SKFS_SOFTWARE/$SKCE_BASE_LDIF \
+                                   --rootUserPassword $SERVICE_LDAP_BIND_PASS \
+                                   --baseDN $SERVICE_LDAP_BASEDN \
+                                   --hostname $(hostname) \
+                                   --ldapPort 1389 \
+                                   --doNotStart \
+                                   --quiet
+        fi
+
+
+        sed -i '/^control-panel/s|$| -Dcom.sun.jndi.ldap.object.disableEndpointIdentification=true|' $OPENDJ_HOME/config/java.properties
+        $OPENDJ_HOME/bin/dsjavaproperties >/dev/null
+
+        cp $SKFS_SOFTWARE/opendjd /etc/init.d/
+        chmod 755 /etc/init.d/opendjd
+        /lib/systemd/systemd-sysv-install enable opendjd
+fi                
 
 ##### Change ownership of files #####
 chown -R strongkey:strongkey $STRONGKEY_HOME
+
+##### Start OpenDJ #####
+if [ $INSTALL_OPENDJ = 'Y' ]; then
+        service opendjd start
+        $OPENDJ_HOME/bin/dsconfig set-global-configuration-prop \
+                                  --hostname $(hostname) \
+                                  --port 4444 \
+                                  --bindDN "cn=Directory Manager" \
+                                  --bindPassword "$SERVICE_LDAP_BIND_PASS" \
+                                  --set check-schema:false \
+                                  --trustAll \
+                                  --no-prompt
+fi
 
 ##### Start MariaDB and Payara #####
 echo -n "Creating $DBSIZE SKFS Internal Database..."
