bdf91a85d10fd65887977cf2ee53726d557db36b
Pushkar Marathe <pushkar.marathe@strongkey.com>
2019-06-12 15:33:11 -0700

    rebasing

--- server/fidoserverbeans/src/main/java/com/strongkey/skfs/txbeans/v1/u2fServletHelperBean_v1.java
+++ server/fidoserverbeans/src/main/java/com/strongkey/skfs/txbeans/v1/u2fServletHelperBean_v1.java
@@ -536,12 +536,9 @@ public class u2fServletHelperBean_v1 implements u2fServletHelperBeanLocal_v1 {
                 responseJSON = FIDO2Regejb.execute(Long.parseLong(did), registrationresponse, registrationmetadata);
             }
         } catch (Exception ex) {
-            if (ex.getCause().getClass() == IllegalArgumentException.class) {
-                skfsLogger.log(skfsConstants.SKFE_LOGGER, Level.SEVERE, "FIDO-ERR-0001", ex.getMessage());
-            }
-            skfsLogger.log(skfsConstants.SKFE_LOGGER, Level.SEVERE, "FIDO-ERR-0001", ex.getMessage());
+            skfsLogger.log(skfsConstants.SKFE_LOGGER, Level.SEVERE, "FIDO-ERR-0001", ex.getCause().getMessage());
             return skfsCommon.buildRegisterResponse(null, "",
-                    skfsCommon.getMessageProperty("FIDO-ERR-0001") + ex.getMessage());
+                    skfsCommon.getMessageProperty("FIDO-ERR-0001") + ex.getCause().getMessage());
         }
         //  9.  Build the output json object
 
