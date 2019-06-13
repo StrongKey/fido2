efc3cebcfdc92b4ea5d6a1eab90eacaa13f91789
Pushkar Marathe <pushkar.marathe@strongkey.com>
2019-06-13 09:45:10 -0700

    rebasing

--- server/fidoserverInstall/install-skfs.sh
+++ server/fidoserverInstall/install-skfs.sh
@@ -90,7 +90,11 @@ APT_GET_CMD=$(which apt-get 2>/dev/null)
 echo "Installing required linux packages ..."
 if [[ ! -z $YUM_CMD ]]; then
     yum -y install unzip libaio java-1.8.0-openjdk ncurses-compat-libs rng-tools curl >/dev/null 2>&1
+<<<<<<< HEAD
     systemctl restart rngd
+=======
+    systemctl restart rng-tools
+>>>>>>> updated gitignore
 elif [[ ! -z $APT_GET_CMD ]]; then
     apt-get update >/dev/null 2>&1
     apt install unzip libaio1 openjdk-8-jdk-headless daemon rng-tools curl -y >/dev/null 2>&1
